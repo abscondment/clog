7e77d91f7016f7d5dfe9565841695fee44c79c1a
abscondment <brendan.ribera@gmail.com>
2010-02-03 12:00:42 -0800

    Removing DB cruft.

--- src/clog/db.clj
+++ src/clog/db.clj
@@ -1,7 +1,5 @@
 (ns clog.db
-  (:use [clojure.contrib.duck-streams])
-  (:require [clojure.contrib.sql :as sql]))
-
+  (:use [clojure.contrib.duck-streams]))
   
 (defn all-posts []
   (reverse
@@ -22,32 +20,3 @@
        3
        (rest
         (read-lines "./public/blog/posts.yaml"))))))))
-
-(comment
-(def db {:classname    "org.postgresql.Driver"
-         :subprotocol  "postgresql"
-         :subname (let [db-host "127.0.0.1"
-                        db-port 5432
-                        db-name "typo"]
-                    (str "//" db-host ":" db-port "/" db-name))
-         :user "brendan"
-         :password ""})
-
-(def select-translation
-     "id, title, body, extended, permalink as url, published_at as created_at")
-
-(defn sql-query [query]
-  (map
-   (fn [r]
-     (merge r {:body ;(.. (new com.petebevin.markdown.MarkdownProcessor)
-                     ;    (markdown
-                          (str (r :body) (r :extended))
-                     ;     ))
-               :created_at (.toString (r :created_at))}))
-   (sql/with-connection db
-     (sql/with-query-results res query (doall res)))))
-
-(defn all-posts []
-  (sql-query
-   [(str "select " select-translation " from contents order by id desc")]))
-)
