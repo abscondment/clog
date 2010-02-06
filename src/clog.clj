cd12eec77149b9c88af99e0fccf0c3aedc9f589d
abscondment <brendan.ribera@gmail.com>
2010-02-05 17:16:03 -0800

    md5 checks to speed up file generation.

--- src/clog.clj
+++ src/clog.clj
@@ -1,4 +1,3 @@
-;(set! *warn-on-reflection* true)
 (ns clog
   (:use [clojure.contrib.duck-streams]
         [clog config db views])
@@ -6,7 +5,7 @@
 
 (defn -main [& args]
   
-   (comment
+  (comment
     (spit "./public/blog/posts.yaml"
           (loop [yaml "---\n"
                  [post & more-posts :as posts] (all-posts)]
@@ -19,17 +18,22 @@
                         "  title: " (post :title) "\n"
                         "  created_at: " (post :created_at) "\n")
                    more-posts))))))
- 
+  
   (let [posts
         (loop [return-posts (list)
                [post & more-posts :as posts] (all-posts)]
           (if (empty? posts) (reverse return-posts)
-              (let [dir (java.io.File. (str "./public/blog/" (post :url)))
-                    _ (if (not (.exists dir)) (.mkdir dir))
-                    _ (spit (str "./public/blog/" (post :url) "/index.html")
-                            (blog-post post
-                                       (first more-posts)
-                                       (first return-posts)))]
+              (let [;_ (if (post :updated) (println (post :url)))
+                    _ (if (post :updated)
+                        (let [dir (java.io.File. (str "./public/blog/" (post :url)))
+                              _ (if (not (.exists dir)) (.mkdir dir))
+                              _ (spit (str "./public/blog/" (post :url) "/index.html")
+                                      (blog-post post
+                                                 (first more-posts)
+                                                 (first return-posts)))
+                              _ (spit (str "./public/blog/" (post :url) "/post.markdown.md5sum")
+                                      (post :md5))
+                              ]))]
                 (recur (conj return-posts post) more-posts))))]
     (do
       (spit "./public/index.html" (blog-index posts))
--- src/clog/db.clj
+++ src/clog/db.clj
@@ -1,25 +1,48 @@
 (ns clog.db
-  (:use [clojure.contrib.duck-streams]))
+  (:use [clojure.contrib.duck-streams])
+  (:import
+   (java.io FileNotFoundException)
+   (java.security NoSuchAlgorithmException MessageDigest)
+   (java.math BigInteger)))
 
-(def *markdown-processor*
-     (new com.petebevin.markdown.MarkdownProcessor))
+;;; From http://www.holygoat.co.uk/blog/entry/2009-03-26-1
+(defn md5-sum
+  "Compute the hex MD5 sum of a string."
+  [#^String str]
+  (let [alg (doto (MessageDigest/getInstance "MD5")
+              (.reset)
+              (.update (.getBytes str)))]
+    (try
+     (.toString (new BigInteger 1 (.digest alg)) 16)
+     (catch NoSuchAlgorithmException e
+       (throw (new RuntimeException e))))))
+
 
 (defn all-posts []
-  (reverse
-   (sort-by
-    :created_at
-    (filter
-     not-empty
-     (map
-      (fn [yaml]
-        (let [url (apply str (butlast (first yaml)))
-              body (slurp (str "./public/blog/" url "/post.markdown"))]
-          (if (not (empty? body))
-            {:url url
-             :title (apply str (drop 9 (fnext yaml)))
-             :created_at (apply str (drop 14 (last yaml)))
-             :body (.. *markdown-processor* (markdown body))})))
-      (partition
-       3
-       (rest
-        (read-lines "./public/blog/posts.yaml"))))))))
+  (let [markdown-processor (new com.petebevin.markdown.MarkdownProcessor)]
+    (reverse
+     (sort-by
+      :created_at
+      (filter
+       not-empty
+       (map
+        (fn [yaml]
+          (let [url (apply str (butlast (first yaml)))
+                exisitng-md5
+                ;(try
+                (slurp (str "./public/blog/" url "/post.markdown.md5sum"))
+                ;(catch FileNotFoundException e ""))
+                body (slurp (str "./public/blog/" url "/post.markdown"))
+                new-md5 (md5-sum body)]
+            (do (println "found" exisitng-md5 "new" new-md5)
+                (if (not (empty? body))
+                  {:url url
+                   :title (apply str (drop 9 (fnext yaml)))
+                   :created_at (apply str (drop 14 (last yaml)))
+                   :body (.. markdown-processor (markdown body))
+                   :md5 new-md5
+                   :updated (not= exisitng-md5 new-md5)}))))
+        (partition
+         3
+         (rest
+          (read-lines "./public/blog/posts.yaml")))))))))
