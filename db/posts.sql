b665dcaf15a345797fd09f7117d5915595a55a55
abscondment <brendan.ribera@gmail.com>
2010-02-15 23:14:35 -0800

    db is defunct; updated ignores

--- db/posts.sql
+++
@@ -1,8 +0,0 @@
-create table posts (
-       id    integer primary key,
-       title varchar(128),
-       body  text,
-       url   varchar(128),
-       created_at text
-);
-create unique index seourl on posts (url);
