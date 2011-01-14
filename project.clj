a40defab328e5e5acac00f2f9f8a70e6ec87f8b4
abscondment <brendan.ribera@gmail.com>
2011-01-14 00:31:50 -0800

    Shell script invocation with style and technique borrowed from Leiningen. Made into SNAPSHOT version because, well, that's what it is after all.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clog "0.5"
+(defproject clog "0.5-SNAPSHOT"
   :repositories {"scala-tools" "http://scala-tools.org/repo-releases/"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
