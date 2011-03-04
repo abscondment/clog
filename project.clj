aeaea8cf9da600c486d06053cca5fd19f0b93386
Brendan Ribera <brendan.ribera@gmail.com>
2011-03-03 19:32:50 -0800

    enlive 1.0.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :repositories {"scala-tools" "http://scala-tools.org/repo-releases/"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [enlive "1.0.0-SNAPSHOT"]
+                 [enlive "1.0.0"]
                  [clj-glob "1.0.0"]
                  [clj-yaml "0.3.0-SNAPSHOT"]
                  [org.markdownj/markdownj "0.3.0-1.0.2b4"]]
