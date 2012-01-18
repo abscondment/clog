19a4640d6600e6a3f376bc0c6a470ecaf7fd3ac6
abscondment <brendan.ribera@gmail.com>
2012-01-18 14:56:39 -0800

    This should just be a raw map. #1

--- src/clog/generate/default/config.clj
+++ src/clog/generate/default/config.clj
@@ -1,10 +1,9 @@
-(def *config*
-  {:title "Example Blog with Text"
-   :subtitle "And Subtext"
-   :author "Marcus Aurelius"
-   :domain "example.org"
-   :page-size 7
-   :static-paths ["/magic/"
-                  "/dwarves/"
-                  "/stoicism/"]
-   })
+{:title "Example Blog with Text"
+ :subtitle "And Subtext"
+ :author "Marcus Aurelius"
+ :domain "example.org"
+ :page-size 7
+ :static-paths ["/magic/"
+                "/dwarves/"
+                "/stoicism/"]
+ }
