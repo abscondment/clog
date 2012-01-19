4325ab47626fb2416895187b75c13936684e551a
Brendan Ribera <brendan.ribera@gmail.com>
2012-01-19 12:27:16 -0800

    Allow different ways of calling tasks. Show task names. Preparing for "clean" task.

--- src/clog/core.clj
+++ src/clog/core.clj
@@ -10,10 +10,13 @@
   
   (let [[options remaining help]
         (cli *command-line-args*
+             ["-t" "--task" "Task to run. Choose from create-blog, new, clean, update." :default nil]
              ["-h" "--help" "Show this help message" :flag true]
              ["-c" "--config" "Optional path to a config file" :default nil])]
 
-    (when (or (:help options) (empty? remaining))
+    (when (or (:help options) (and
+                               (empty? (:task options))
+                               (empty? remaining)))
       ;; print help and quit
       (println help)
       (System/exit 0))
@@ -27,11 +30,14 @@
          
       ;; run command
       (let [[command & extras] remaining
-            extras-string (apply str (butlast (interleave extras (repeat " "))))]
+            extras-string (apply str (butlast (interleave extras (repeat " "))))
+            command (or (:task options) command)
+            remaining (if (:task options) (cons command remaining) remaining)]
         (case command
           "create-blog" (generate/create-blog (or extras-string "."))
           "new" (generate/post extras-string)
-          "update" (update/site)))
+          "update" (update/site)
+          "clean" (update/clean)))
          
       ;; shut down threadpool from pmap et al.
       (shutdown-agents))))
